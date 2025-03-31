65093ee34e096bafbe6801b71362838f0b9de7a4
lokesh220 <lokeshchoraria2@gmail.com>
2025-03-31 15:22:48 +0530

    SECRETS SECURED

--- Assignment-api/assignment-portal/database/postgres.go
+++ Assignment-api/assignment-portal/database/postgres.go
@@ -2,23 +2,39 @@ package database
 
 import (
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/lokesh2201013/assignment-portal/models"
-"gorm.io/driver/postgres"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func ConnectDB(){
-	 dsn := "host=localhost user=postgres password= 9910994194lokesh dbname= assignment port= 5432 sslmode=disable"
-     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-	if err!=nil{
-		log.Fatalf("Failed to connect to db ,err = %v\n",err)
-
+func ConnectDB() {
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: No .env file found")
 	}
-	db.AutoMigrate(&models.User{})
-	db.AutoMigrate(&models.Assignment{})
-	DB=db
+
+	dsn := "host=" + os.Getenv("DB_HOST") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" port=" + os.Getenv("DB_PORT") +
+		" sslmode=disable"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to db: %v\n", err)
+	}
+
+	// Migrate all models in one call
+	if err := db.AutoMigrate(&models.User{}, &models.Assignment{}); err != nil {
+		log.Fatalf("AutoMigrate failed: %v\n", err)
+	}
+
+	DB = db
 	log.Println("Connected to PostgreSQL using GORM")
 }
